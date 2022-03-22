440b7e0df4275490da00bf71e52e5ba4370c970f
Mausam-1302 <94812867+Mausam-1302@users.noreply.github.com>
2022-03-22 15:58:38 -0400

    lab3

---
+++ powershell/lab3_networkinfo.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Network information:"
+get-ciminstance Win32_NetworkAdapterconfiguration |
+where {$_.ipenabled -eq "True" } | 
+Format-Table Description,
+Index,
+IPAddress,
+IPSubnet,
+@{n="DNSHostName";e={switch($_.DNSDomain){$Null{$myvar="No information found here."; $myvar}};if($Null -ne $_.DNSDomain){$_.DNSDomain}}},
+@{n="DNSServerOrder";e={switch($_.DNSServersearchorder){$Null{$myvar="No information found here."; $myvar}};if($Null -ne $_.DNSServersearchorder){$_.DNSServersearchorder}}}
